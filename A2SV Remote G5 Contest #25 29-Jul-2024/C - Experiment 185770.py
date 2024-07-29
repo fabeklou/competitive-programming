f479f5877ce1e881640b6383121562c8748b4ff5
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-07-29 18:55:01 +0000

    Created 'C - Experiment' by A2SV Hub

---
+++ A2SV Remote G5 Contest #25 29-Jul-2024/C - Experiment 185770.py
@@ -0,0 +1,31 @@
+# Problem: C - Experiment - https://codeforces.com/gym/537575/problem/C
+
+import heapq
+
+exps = []
+last_exp = first_exp = 0
+
+for _ in range(int(input())):
+    start, end, rooms = map(int, input().split())
+    exps.append((start, end, rooms))
+    last_exp = max(last_exp, start)
+    first_exp = min(first_exp, start)
+
+heapq.heapify(exps)
+running_exps = []
+max_rooms = 0
+curr_rooms = 0
+
+for time in range(first_exp, last_exp + 1):
+    while exps and exps[0][0] == time:
+        _, end, rooms = heapq.heappop(exps)
+        heapq.heappush(running_exps, (end + 1, rooms))
+        curr_rooms += rooms
+
+    while running_exps and running_exps[0][0] == time:
+        _, rooms = heapq.heappop(running_exps)
+        curr_rooms -= rooms
+
+    max_rooms = max(max_rooms, curr_rooms)
+
+print(max_rooms)
