c1ccba53f78025def39971c55a5e61e3f6f2e98d
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-07-29 18:54:05 +0000

    Created 'A - Valid Parentheses' by A2SV Hub

---
+++ A2SV Remote G5 Contest #25 29-Jul-2024/A - Valid Parentheses 185767.py
@@ -0,0 +1,17 @@
+# Problem: A - Valid Parentheses - https://codeforces.com/gym/537575/problem/A
+
+string  = input()
+stack = []
+count = 0
+
+for char in string:
+    if char == '(':
+        stack.append(char)
+    else:
+        if len(stack) == 0:
+            pass
+        else:
+            stack.pop()
+            count += 1
+            
+print(count*2)
