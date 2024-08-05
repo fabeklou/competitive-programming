a98725079c0cc42b51cea7165a0bbd8c31d8a024
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-08-05 18:23:52 +0000

    Created 'B - Chat room' by A2SV Hub

---
+++ A2SV Remote Contest #26 05-Aug-2024/B - Chat room 187626.py
@@ -0,0 +1,14 @@
+# Problem: B - Chat room - https://codeforces.com/gym/540348/problem/B
+
+from collections import deque
+
+word = input()
+expected = deque(['h', 'e', 'l', 'l', 'o'])
+
+for char in word:
+    if char == expected[0]:
+        expected.popleft()
+        if not expected:
+            break
+
+print("YES" if not expected else "NO")
