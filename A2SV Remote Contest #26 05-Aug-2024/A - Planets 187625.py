0fc790f656d72e856f40d25eab23266940ab0241
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-08-05 18:23:43 +0000

    Created 'A - Planets' by A2SV Hub

---
+++ A2SV Remote Contest #26 05-Aug-2024/A - Planets 187625.py
@@ -0,0 +1,14 @@
+# Problem: A - Planets - https://codeforces.com/gym/540348/problem/A
+
+from collections import Counter
+
+for _ in range(int(input())):
+    n, c = map(int, input().split())
+    orbits = list(map(int, input().split()))
+    
+    count = 0
+    uniq_orbit = Counter(orbits)
+    for orbit, number in uniq_orbit.items():    
+        count += min(c, number)
+
+    print(count)
