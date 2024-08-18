9f79afc81868935cb068e98a87c1ee1ce116f6b7
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-08-18 20:45:08 +0000

    Time: 39 ms (27.41%), Space: 16.7 MB (8.92%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,21 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits: return []
+
+        keyboard = { "2": "abc", "3": "def", "4": "ghi", "5": "jkl",
+            "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"
+        }
+
+        res = []
+        def bt(index, curr):
+            if len(digits) == len(curr):
+                res.append(''.join(curr))
+                return
+            
+            for ch in keyboard[digits[index]]:
+                curr.append(ch)
+                bt(index + 1, curr)
+                curr.pop()
+
+        bt(0, [])
+        return res
