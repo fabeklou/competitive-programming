96e0ba92237124c2546afb74c3c4f25d8fdfa66d
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-08-23 19:59:13 +0000

    Created 'Find the Index of the first occurence' by A2SV Hub

---
+++ 23-Aug-2024/Find the Index of the first occurence 204348.py
@@ -0,0 +1,8 @@
+# Problem: Find the Index of the first occurence - https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        try:
+            return haystack.index(needle)
+        except:
+            return -1
