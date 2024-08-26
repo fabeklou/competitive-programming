b7bb0c165a63935b6ad1faf93f061076deab287a
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-08-26 18:41:24 +0000

    Created 'A - Maximum Increase' by A2SV Hub

---
+++ A2SV Remote G5 Education Contest #28 26-Aug-2024/A - Maximum Increase 204896.py
@@ -0,0 +1,15 @@
+# Problem: A - Maximum Increase - https://codeforces.com/gym/543262/problem/A
+
+n = int(input())
+a = list(map(int, input().split()))
+
+maxi = 0
+curr = 0
+for i in range(0, n-1):
+    if a[i] < a[i+1]:
+        curr += 1
+    else:
+        curr = 0
+    maxi = max(maxi, curr)
+
+print(maxi+1)
