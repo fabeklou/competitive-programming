cae81da554f2ab72f853c07411bd91f91e4a788c
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-08-26 18:41:25 +0000

    Created 'B - Make it Divisible by 25' by A2SV Hub

---
+++ A2SV Remote G5 Education Contest #28 26-Aug-2024/B - Make it Divisible by 25 204897.py
@@ -0,0 +1,31 @@
+# Problem: B - Make it Divisible by 25 - https://codeforces.com/gym/543262/problem/B
+
+for _ in range(int(input())):
+    n = input()
+
+    min_steps = float('inf')
+    length = len(n)
+
+    # Check for each target pair: "00", "25", "50", "75"
+    targets = ["00", "25", "50", "75"]
+
+    for target in targets:
+        pos2 = -1
+        pos1 = -1
+        
+        for index in range(length - 1, -1, -1):
+            if n[index] == target[1]:
+                pos2 = index
+                break
+        
+        if pos2 != -1:
+            for index in range(pos2 - 1, -1, -1):
+                if n[index] == target[0]:
+                    pos1 = index
+                    break
+        
+        if pos1 != -1 and pos2 != -1:
+            steps = (length - pos2 - 1) + (pos2 - pos1 - 1)
+            min_steps = min(min_steps, steps)
+    
+    print(min_steps)
