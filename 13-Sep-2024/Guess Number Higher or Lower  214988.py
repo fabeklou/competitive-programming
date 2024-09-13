19dda2bc55d515600b4e06e186fd7a6662f49d42
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-09-13 21:02:19 +0000

    Created 'Guess Number Higher or Lower ' by A2SV Hub

---
+++ 13-Sep-2024/Guess Number Higher or Lower  214988.py
@@ -0,0 +1,22 @@
+# Problem: Guess Number Higher or Lower  - https://leetcode.com/problems/guess-number-higher-or-lower/
+
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        left, right = 1, n
+        while left <= right:
+            mid = (right - left) // 2 + left
+            print(mid, left, right)
+            verdict = guess(mid)
+            if verdict == -1:
+                right = mid - 1
+            elif verdict == 1:
+                left = mid + 1
+            else:
+                return mid
