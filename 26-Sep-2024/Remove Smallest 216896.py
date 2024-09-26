5f2873494c93908b3b40bc5e4e16a039c1f9ba9e
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-09-26 20:44:15 +0000

    Created 'Remove Smallest' by A2SV Hub

---
+++ 26-Sep-2024/Remove Smallest 216896.py
@@ -0,0 +1,13 @@
+# Problem: Remove Smallest - https://codeforces.com/problemset/problem/1399/A
+
+from collections import deque
+
+for _ in range(int(input())):
+    n = int(input())
+    a = deque(sorted(map(int, input().split())))
+    while len(a) > 1:
+        if a[1] - a[0] <= 1:
+            a.popleft()
+        else:
+            break
+    print('YES' if len(a) == 1 else 'NO')
