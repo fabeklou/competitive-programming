478b01e2fc27f6a9c2803532001b59c842a8dc93
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-10-20 22:59:39 +0000

    Created 'Find All Numbers Disappeared in an Array' by A2SV Hub

---
+++ 20-Oct-2024/Find All Numbers Disappeared in an Array 220906.py
@@ -0,0 +1,5 @@
+# Problem: Find All Numbers Disappeared in an Array - https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/description/
+
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        return list(set(nums) ^ set(range(1, len(nums) + 1)))
