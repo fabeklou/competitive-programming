8c5c26775e757f12bf2aa9036fd6322564b8d47e
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-10-23 22:57:16 +0000

    Created 'Where's the Bishop?' by A2SV Hub

---
+++ 23-Oct-2024/Where's the Bishop? 221897.py
@@ -0,0 +1,19 @@
+# Problem: Where's the Bishop? - https://codeforces.com/problemset/problem/1692/C
+
+def solve():
+    matrix = []
+    for i in range(9):
+        row = list(input())
+        if i != 0:
+            matrix.append(row)
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])-2):
+            if matrix[i][j] == matrix[i][j+2] == "#":
+                ans = [i+2, j+2]
+                print(*ans)
+                return  
+ 
+ 
+t = int(input())
+for _ in range(t):
+    solve()
