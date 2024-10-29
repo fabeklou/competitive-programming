e47ab62152892a90253b573abfb8f862ccdd237d
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-10-29 17:05:18 +0000

    Created 'Young Physicist' by A2SV Hub

---
+++ 29-Oct-2024/Young Physicist 222485.py
@@ -0,0 +1,9 @@
+# Problem: Young Physicist - https://codeforces.com/problemset/problem/69/A
+
+x = y = z = 0
+for _ in range(int(input())):
+    a, b, c = map(int, input().split(' '))
+    x += a
+    y += b
+    z += c
+print('YES' if x == y == z == 0 else 'NO')
