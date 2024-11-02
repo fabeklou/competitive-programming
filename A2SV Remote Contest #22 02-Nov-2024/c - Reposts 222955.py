5852fd9d476a8f1be653082890b564331f115f34
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-11-02 22:33:22 +0000

    Created 'c - Reposts' by A2SV Hub

---
+++ A2SV Remote Contest #22 02-Nov-2024/c - Reposts 222955.py
@@ -0,0 +1,19 @@
+# Problem: c - Reposts - https://codeforces.com/gym/533316/problem/c
+
+from collections import defaultdict
+
+n = int(input())
+graph = defaultdict(list)
+for _ in range(n):
+    name1, _, name2 = input().split()
+    name1, name2 = name1.lower(), name2.lower()
+    graph[name2].append(name1)
+
+def dfs(node):
+    max_length = 1
+    for neighbor in graph[node]:
+        max_length = max(max_length, 1 + dfs(neighbor))
+    return max_length
+
+result = dfs('polycarp')
+print(result)
