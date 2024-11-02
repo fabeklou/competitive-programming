72b791a789d4141e1c4b26b47d79da5e0ae316e1
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-11-02 22:32:33 +0000

    Created 'A - Rudolph and Cut the Rope ' by A2SV Hub

---
+++ A2SV Remote Contest #22 02-Nov-2024/A - Rudolph and Cut the Rope  222953.py
@@ -0,0 +1,15 @@
+# Problem: A - Rudolph and Cut the Rope  - https://codeforces.com/gym/533316/problem/A
+
+for _ in range(int(input())):
+    n = int(input())
+    a = []
+    for _ in range(n):
+        x, y = map(int, input().split())
+        a.append(x - y)
+    a.sort(reverse=True)
+    res = 0
+    for index in range(len(a)):
+        if a[index] <= 0:
+            break
+        res += 1
+    print(res)
