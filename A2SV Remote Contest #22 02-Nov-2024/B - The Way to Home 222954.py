25b47675e9e1447101ad70fd155f18e34b85100e
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-11-02 22:33:10 +0000

    Created 'B - The Way to Home' by A2SV Hub

---
+++ A2SV Remote Contest #22 02-Nov-2024/B - The Way to Home 222954.py
@@ -0,0 +1,21 @@
+# Problem: B - The Way to Home - https://codeforces.com/gym/533316/problem/B
+
+n, d = map(int, input().split())
+s = input()
+
+pos = 0
+jumps = 0
+can_reach = True
+
+while pos != n - 1:
+    next_pos = -1
+    for i in range(pos + 1, min(pos + d + 1, n)):
+        if s[i] == '1':
+            next_pos = i
+    if next_pos == -1:
+        can_reach = False
+        break
+    pos = next_pos
+    jumps += 1
+
+print(jumps if can_reach else -1)
