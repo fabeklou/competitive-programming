604743405becc4f21c507e439e58cea20a641be1
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-11-26 22:18:39 +0000

    Created 'A - Maximum Multiple Sum' by A2SV Hub

---
+++ A2SV Remote Contest #34 26-Nov-2024/A - Maximum Multiple Sum 224382.py
@@ -0,0 +1,13 @@
+# Problem: A - Maximum Multiple Sum - https://codeforces.com/gym/559568/problem/A
+
+for _ in range(int(input())):
+    n =  int(input())
+    result = 2
+    sm = 2
+    for val1 in range(2, n + 1):
+        curr = 0
+        for val2 in range(val1, n + 1, val1):
+            curr += val2
+        if curr > sm:
+            result, sm = val1, curr
+    print(result)
