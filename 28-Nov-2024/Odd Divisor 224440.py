9176fb214bb9a4db83dc66acc640e3df3d33760e
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-11-28 21:58:37 +0000

    Created 'Odd Divisor' by A2SV Hub

---
+++ 28-Nov-2024/Odd Divisor 224440.py
@@ -0,0 +1,12 @@
+# Problem: Odd Divisor - https://codeforces.com/problemset/problem/1475/A
+
+for _ in range(int(input())):
+    num = int(input())
+    result = False
+    div = num
+    while div > 1:
+        if div % 2 == 1 and num % div == 0:
+            result = True
+            break
+        div //= 2
+    print("YES" if result else "NO")
