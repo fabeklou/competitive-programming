43b139a067c1bd0d125f7009bac40b7c0138f7d6
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-11-29 09:13:55 +0000

    Created ' Divisible Array' by A2SV Hub

---
+++ 29-Nov-2024/ Divisible Array 224448.py
@@ -0,0 +1,6 @@
+# Problem:  Divisible Array - https://codeforces.com/contest/1828/problem/A
+
+for _ in range(int(input())):
+    n = int(input())
+    result = [(index + 1) * 2 for index in range(n)]
+    print(*result)
